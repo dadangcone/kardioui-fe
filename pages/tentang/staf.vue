99ea7a5bca61ce504b5f5b295e995b8ff2732a9d
rexy pratama <pratamarexy93@gmail.com>
2021-11-22 14:29:35 +0700

    fix search

--- pages/tentang/staf.vue
+++ pages/tentang/staf.vue
@@ -18,8 +18,7 @@
                           type="search"
                           class="search-list left big"
                           v-model="search"
-                          placeholder="Cari staf pengajar dan bidangnya"
-                          v-on:keyup.enter="searchStaff"
+                          placeholder="Cari staf pengajar"
                         ></b-form-input>
                         <div class="sort d-none">
                           <label for="">Filter berdasarkan</label>
@@ -33,8 +32,11 @@
                           </b-form-select>
                         </div>
                         <div class="clearfix"></div>
-                        <b-row>
-                          <b-col cols="12">
+                        <span v-if="dataStaff.length === 0"
+                          >Hasil Pencarian Tidak ditemukan..</span
+                        >
+                        <b-row v-else>
+                          <b-col cols="12" v-if="dataStaff['Aritmia']">
                             <h5 class="mb-3">Aritmia</h5></b-col
                           >
                           <b-col
@@ -47,7 +49,14 @@
                               <!-- <h6>{{ data.divisi }}</h6> -->
                             </div>
                           </b-col>
-                          <b-col cols="12">
+                          <b-col
+                            cols="12"
+                            v-if="
+                              dataStaff[
+                                'Kardiologi Intensif dan Kegawatan Kardiovaskular'
+                              ]
+                            "
+                          >
                             <h5 class="mb-3 mt-3">
                               Kardiologi Intensif dan Kegawatan Kardiovaskular
                             </h5></b-col
@@ -64,7 +73,14 @@
                               <!-- <h6>{{ data.divisi }}</h6> -->
                             </div>
                           </b-col>
-                          <b-col cols="12">
+                          <b-col
+                            cols="12"
+                            v-if="
+                              dataStaff[
+                                'Kardiologi Invasif dan Intervensi Non bedah'
+                              ]
+                            "
+                          >
                             <h5 class="mb-3 mt-3">
                               Kardiologi Invasif dan Intervensi Non bedah
                             </h5></b-col
@@ -81,7 +97,10 @@
                               <!-- <h6>{{ data.divisi }}</h6> -->
                             </div>
                           </b-col>
-                          <b-col cols="12">
+                          <b-col
+                            cols="12"
+                            v-if="dataStaff['Kardiologi Klinik']"
+                          >
                             <h5 class="mb-3 mt-3">
                               Kardiologi Klinik
                             </h5></b-col
@@ -96,7 +115,14 @@
                               <!-- <h6>{{ data.divisi }}</h6> -->
                             </div>
                           </b-col>
-                          <b-col cols="12">
+                          <b-col
+                            cols="12"
+                            v-if="
+                              dataStaff[
+                                'Kardiologi Non-Invasif Ekokardiografi dan Treadmill'
+                              ]
+                            "
+                          >
                             <h5 class="mb-3 mt-3">
                               Kardiologi Non-Invasif Ekokardiografi dan
                               Treadmill
@@ -114,7 +140,14 @@
                               <!-- <h6>{{ data.divisi }}</h6> -->
                             </div>
                           </b-col>
-                          <b-col cols="12">
+                          <b-col
+                            cols="12"
+                            v-if="
+                              dataStaff[
+                                'Kardiologi Pediatrik dan Penyakit Jantung Bawaan'
+                              ]
+                            "
+                          >
                             <h5 class="mb-3 mt-3">
                               Kardiologi Pediatrik dan Penyakit Jantung Bawaan
                             </h5></b-col
@@ -131,7 +164,10 @@
                               <!-- <h6>{{ data.divisi }}</h6> -->
                             </div>
                           </b-col>
-                          <b-col cols="12">
+                          <b-col
+                            cols="12"
+                            v-if="dataStaff['Kedokteran Vaskular']"
+                          >
                             <h5 class="mb-3 mt-3">
                               Kedokteran Vaskular
                             </h5></b-col
@@ -146,7 +182,14 @@
                               <!-- <h6>{{ data.divisi }}</h6> -->
                             </div>
                           </b-col>
-                          <b-col cols="12">
+                          <b-col
+                            cols="12"
+                            v-if="
+                              dataStaff[
+                                'Prevensi dan Rehabilitasi Kardiovaskular'
+                              ]
+                            "
+                          >
                             <h5 class="mb-3 mt-3">
                               Prevensi dan Rehabilitasi Kardiovaskular
                             </h5></b-col
@@ -234,6 +277,8 @@ export default {
     return {
       sort: "",
       search: "",
+      listWatch: false,
+      sourceCancel: null,
       current_page: 1,
       last_page: 0,
       dataStaff: {},
@@ -256,6 +301,17 @@ export default {
   mounted() {
     this.getStaff();
   },
+  watch: {
+    search: {
+      handler() {
+        if (this.listWatch) {
+          this.sourceCancel.cancel();
+          this.getStaff();
+        }
+      },
+      deep: true
+    }
+  },
   computed: {
     compare() {
       return this.current_page >= this.last_page;
@@ -263,6 +319,31 @@ export default {
   },
   methods: {
     async getStaff() {
+      const { CancelToken } = this.$axios;
+      this.sourceCancel = CancelToken.source();
+
+      let tempStaff = await this.$axios.$get(
+        `/staff-pengajar-new?search=` +
+          this.search.toLowerCase() +
+          "&page=" +
+          this.current_page,
+        {
+          cancelToken: this.sourceCancel.token
+        }
+      );
+
+      // this.dataStaff.push.apply(this.dataStaff, tempStaff.data.data);
+      // this.last_page = tempStaff.data.last_page;
+
+      this.listWatch = true;
+      this.dataStaff = tempStaff.data;
+    },
+    async searchStaff() {
+      const { CancelToken } = this.$axios;
+      this.sourceCancel = CancelToken.source();
+
+      this.dataStaff = [];
+      this.current_page = 1;
       let tempStaff = await this.$axios.$get(
         `/staff-pengajar-new?search=` +
           this.search.toLowerCase() +
@@ -270,35 +351,8 @@ export default {
           this.current_page
       );
 
-      // this.dataStaff.push.apply(this.dataStaff, tempStaff.data.data);
-      // this.last_page = tempStaff.data.last_page;
-
+      this.listWatch = true;
       this.dataStaff = tempStaff.data;
-    },
-    async loadStaff() {
-      this.current_page += 1;
-      let tempStaff = await this.$axios.$get(
-        `/staff-pengajar?search=` +
-          this.search.toLowerCase() +
-          "&page=" +
-          this.current_page
-      );
-
-      this.dataStaff.push.apply(this.dataStaff, tempStaff.data.data);
-      this.last_page = tempStaff.data.last_page;
-    },
-    async searchStaff() {
-      this.dataStaff = [];
-      this.current_page = 1;
-      let tempStaff = await this.$axios.$get(
-        `/staff-pengajar?search=` +
-          this.search.toLowerCase() +
-          "&page=" +
-          this.current_page
-      );
-
-      this.dataStaff.push.apply(this.dataStaff, tempStaff.data.data);
-      this.last_page = tempStaff.data.last_page;
     }
   }
 };
