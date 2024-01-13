363f491ca501555198d0b7e179d71dc8564a37b0
Rexy <rexy@Rexys-MacBook-Pro.local>
2024-01-13 19:32:46 +0700

    add googletag

--- components/Header.vue
+++ components/Header.vue
@@ -65,6 +65,16 @@
   </header>
 </template>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4HKZLR9QT7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4HKZLR9QT7');
+</script>
+
 <script>
 import _ from "lodash";
 
