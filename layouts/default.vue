c345b126f13997159f24a7200ba3c182a91f39f3
faris <farissyaifuddin30@gmail.com>
2024-01-17 08:28:15 +0700

    gtag

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,5 +4,12 @@
     <Nuxt />
     <Footer />
     <!-- <ShortContact /> -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4HKZLR9QT7"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-4HKZLR9QT7');
+    </script>
   </div>
 </template>
